30647c9162e5f284e8ec990088af996a2018051e
Kevin Jennison <kevin.jennison1@gmail.com>
2014-05-03 14:27:29 -0700

    Add core models: recipient, campaign, membership, and group.

--- sms_campaigns/sms_main/models.py
+++ sms_campaigns/sms_main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Recipient(models.Model):
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=30)
+    campaigns = models.ManyToManyField('Campaign', through='Membership')
+
+class Membership(models.Model):
+    def __unicode__(self):
+        return str(self.id)
+    recipient = models.ForeignKey(Recipient)
+    campaign = models.ForeignKey('Campaign')
+    time_joined = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+class Campaign(models.Model):
+    def __unicode__(self):
+        return self.name
+    
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    group = models.ForeignKey('Group')
+    message_interval_in_seconds = models.BigIntegerField()
+    total_message_occurrences = models.IntegerField(default=1, help_text="Use zero for infinite occurences.")
+    message_text = models.TextField()
+    
+class Group(models.Model):
+    def __unicode__(self):
+        return self.name
+    
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
--- sms_campaigns/sms_main/views.py
+++ sms_campaigns/sms_main/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from sms_main.models import *
+
+def addGroup(group_name):
+    Group.objects.create(name=group_name)
+    
+def addCampaign(name, description, groupId, message_interval, repeats):
+    group = Group.objects.get(id=groupId)
+    Campaign.objects.create(name=name, description=description, group=group, message_interval_in_seconds=message_interval, total_message_occurrences=repeats)
+    
+def addRecipient(first_name, last_name, phone_number):
+    Recipient.objects.create(first_name=first_name, last_name=last_name, phone_number=phone_number)
+
