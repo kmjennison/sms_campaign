30407c6ec38932d3506f7f2ea8226d88b5a491fa
Kevin Jennison <kevin.jennison1@gmail.com>
2014-05-17 13:58:49 -0700

    Add user profile to admin.

--- sms_campaigns/sms_main/admin.py
+++ sms_campaigns/sms_main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from sms_main.models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 class RecipientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number')
@@ -12,7 +14,21 @@ class CampaignAdmin(admin.ModelAdmin):
     # Recipient.objects.get(id=recipient)
     list_display = ('name', 'group', 'message_text', 'message_interval_in_seconds', 'total_message_occurrences')
 
+# Define an inline admin descriptor for UserProfile model
+# which acts a bit like a singleton
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
 admin.site.register(Recipient, RecipientAdmin)
 admin.site.register(Membership, MembershipAdmin)
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Group)
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
